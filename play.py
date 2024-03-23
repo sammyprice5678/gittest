c3346c64891e65ae48aa67d6d92ee15b0b4b9f07
sammyprice5678 <captsamuelprice75@gmail.com>
2024-03-23 07:34:52 +0100

    updated the play.py script

--- play.py
+++ play.py
@@ -1,28 +1,52 @@
 # rock, paper, scissors
 
 import random
-# we create a list of the options to pick from.
 
-play = ['rock','paper', 'scissors']
+# we create a while loop that allows us to continue playing until were done
+gameon = True
 
-print("welcome To Rock Paper Scissors")
+while gameon:
+    # we create a list of the options to pick from.
 
-# we make provision to allow the player and the computer to choose their option
+    play = ['rock','paper', 'scissors']
 
-your_choice = input("you choose ..: ")
-computer_choice = random.choice(play)
+    print("welcome To Rock Paper Scissors")
 
-print(f"you chose {your_choice} and computer chose {computer_choice}")
+    # we make provision to allow the player and the computer to choose their option
 
-# we make an if statement to ensure the conditions are met before declaring the winner
+    your_choice = input("you choose ..: ")
+    computer_choice = random.choice(play)
 
-if your_choice == computer_choice:
-    print("Its a tie, we have no winner")
-elif your_choice == 'rock' and computer_choice == 'scissors':
-    print("You win")
-elif your_choice == 'scissors' and computer_choice == 'paper':
-    print("you win")
-elif your_choice == 'paper' and computer_choice == 'rock':
-    print("You win")
-else:
-    print("you lose")
+    print(f"you chose {your_choice} and computer chose {computer_choice}")
+
+    # we make an if statement to ensure the conditions are met before declaring the winner
+
+    if your_choice == computer_choice:
+        print("Its a tie, we have no winner")
+    elif your_choice == 'rock' and computer_choice == 'scissors':
+        print("You win")
+    elif your_choice == 'scissors' and computer_choice == 'paper':
+        print("you win")
+    elif your_choice == 'paper' and computer_choice == 'rock':
+        print("You win")
+    else:
+        print("you lose")
+
+    #make a while loop to keep asking for a valid input
+    while True:
+        retry = input("would you like to continue :")
+        if retry == 'yes':
+            break
+        elif retry == 'no':
+            break
+        else:
+            continue
+
+    # once the game is finished, ask if the user is done or would like to continue
+
+    if retry== 'yes':
+        gameon
+    elif retry== 'no':
+        print("good bye")
+        gameon = False
+
